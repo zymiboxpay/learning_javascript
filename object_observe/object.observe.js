9afe9bcc235427ea7926221e73ed9d1960cb12e2
unknown <jude.zhu@LXN7010BTZRH42.Optilink.local>
2015-05-29 13:39:50 +0800

    update.

--- object_observe/object.observe.js
+++ object_observe/object.observe.js
@@ -6,3 +6,28 @@ Object.observe(model, function(changes) {
 		console.log(change.type, change.name, change.oldValue);
 	})
 })
+
+var todoModel = window.todoModel = {
+	label: "Default",
+	completed: false
+};
+
+function observer(changes){
+	changes.forEach(function(change, i){
+		console.log("property changed: ", change.name);
+		console.log("type of  change: ", change.type);
+		console.log("value before change: ", change.oldValue);
+		console.log("value after change: ", change.object[change.name]);
+		console.log(change);
+	})
+}
+
+
+Object.observe(todoModel, observer, /*optAcceptList*/['update']);
+
+todoModel.label = "Buy some more milk";
+
+todoModel.completed = "2015/05/29";
+
+Object.unobserve(todoModel, observer);
+
